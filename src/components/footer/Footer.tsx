5cd01956d20c4d3079ff2a675cf0bf8d648eafdb
Ugo Venture <venture.ugo@gmail.com>
2022-12-17 18:56:09 +0100

    download svg

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Footer component
+ * 
+ * @name Footer
+ * 
+ * @returns {JSX.Element} Footer component
+ */
+export const Footer = (): JSX.Element => {
+    return (
+        <footer>
+            
+            <p>&copy; 2021 Company, Inc.</p>
+        </footer>
+    );
+}
